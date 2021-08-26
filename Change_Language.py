0f255561c2a2dd3909f93b75caf22592ec415475
Tommy Wilbur <twilbur@umich.edu>
2021-08-26 13:36:06 -0700

    Added English and German to program, added some more info to README.

--- Change_Language.py
+++ Change_Language.py
@@ -1,78 +1,112 @@
 from os import listdir, mkdir, getcwd, chdir
-from os.path import join, isfile, exists
+from os.path import join, isfile, exists, isdir
+from shutil import copy2
+
+
+D2_packages = "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages"
+lang_dir = "languages"
 
 
 '''
+Thanks to others who have explored local language hacks with Destiny 2's files
+https://steamcommunity.com/app/1085660/discussions/0/1628539187763929500
 
+Download the latest Python here:
+https://www.python.org/downloads/
 
-'''
-
-# File locations found based on
-# https://steamcommunity.com/app/1085660/discussions/0/1628539187763929500
-'''
-C:\Program Files (x86)\Steam\steamapps\common\Destiny 2
 C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages
 
-Just Audio
 
-Exploration:
-
-"Audio" search in packages reveals
-in English: X files
-in Spanish: X files
-in German: X files
-
-"en" reveals
-"de" reveals extras - city defense
-"sp" reveals
-
-
-Just Text
-
-
-
-
-'''
-
-'''
-How to tell which languages the user wants to use?
-How to create directories for each of the text and audio for each language?
-
-Maybe one program to create a backup of the current language,
+Likely one program to create a backup of the current language,
 and another program to actually shift between established backups
 
-
+Biggest issue is how to change text in the UI without changing Steam settings
 
 '''
 
 
+def backup_german_audio_files():
+    # Starting with just copying English audio files
+    de_dir = "german_audio"
+    if not isdir(de_dir):
+        mkdir(de_dir)
 
-# Prompt User for Text Interface, Audio, or both
+    file_list = listdir(D2_packages)
+    file_list_de = [x for x in file_list if "de" in x]
+    file_list_audio = [x for x in file_list if "audio" in x]
+    file_set = set(file_list_de) & set(file_list_audio)
+
+    pack_prefix = D2_packages+"\\" # Need to use double \ to escape just one
+    lang_prefix = D2_packages.replace("packages", lang_dir)+"\\"+de_dir+"\\"
+    for file in file_set:
+        file_src = pack_prefix+file
+        file_dest = lang_prefix+file
+        copy2(file_src, file_dest)
+
+
+def backup_english_audio_files():
+    # Starting with just copying English audio files
+    en_dir = "english_audio"
+    if not isdir(en_dir):
+        mkdir(en_dir)
+
+    file_list = listdir(D2_packages)
+    file_list_en = [x for x in file_list if "en" in x]
+    file_list_audio = [x for x in file_list if "audio" in x]
+    file_set = set(file_list_en) & set(file_list_audio)
+
+    pack_prefix = D2_packages+"\\" # Need to use double \ to escape just one
+    lang_prefix = D2_packages.replace("packages", lang_dir)+"\\"+en_dir+"\\"
+    for file in file_set:
+        file_src = pack_prefix+file
+        file_dest = lang_prefix+file
+        # print(file_src, "\n", file_dest, "\n\n")
+        copy2(file_src, file_dest)
+
 
 def create_language_backup():
-    # Make a new directory
+    print("\nCreating Language Backup...\n\nThis may take up to 10 seconds...\n\n\n")
 
-
-    # Copy the audio files over
-
-    # Copy the text files over
-
-
-
-# Changes directory to D2 packages. Returns nothing
-def cd_D2_packages():
-    D2_packages = "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages"
-
-    print("Current directory:", getcwd())
+    # Check for languages directory
     chdir(D2_packages)
-    print("Current directory:", getcwd())
+    chdir("..")
+    if not isdir(lang_dir):
+        mkdir(lang_dir)
+
+    copy_wanted = input("\n\nWould you like to backup the current audio files? (y/n): ")
+    count = 0
+    skip_copy = False
+    while copy_wanted!="y":
+        if copy_wanted == "n":
+            skip_copy = True
+            break
+        copy_wanted = input("\nInvalid Response... \nWould you like to backup the current audio files? (y/n): ")
+        count +=1
+        if count>2:
+            print("Invalid input 3 times in a row, exiting program")
+            sys.exit()
+
+    if not skip_copy:
+        print("You've chosen to backup the current audio files. \n")
+        # lang_selected = input("What language are you currently using? (en, de, sp):")
+
+        # Copy the audio files over
+        chdir(lang_dir)
+        backup_english_audio_files()
+
+    else:
+        print("\nSkipping backup step")
+
+    # TODO:
+    # Copy the text files over
 
 
 def main():
     print("Welcome to the Destiny 2 Local Language Changer!")
-
     print("Let's change directories to the Destiny 2 packages\n\n")
-    cd_D2_packages()
+
+    create_language_backup()
+
 
 
 
--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # Destiny 2 Language Changer
 
-You're here because you don't want to toggle the Steam settings every time to change between langauges.
+You're here because you don't want to toggle the Steam settings every time to change between languages.
+It requires a download every time, and simply deletes the local files instead of  moving them around for some reason.
+
+Heads up - this program only changes the audio, and doesn't change subtitles or the user interface.
+
+Maybe that can come in a later patch, but right now it's tough with Steam settings.
+
 
 You'll need to download Python to run this program:
 
 Download the latest Python here:
 https://www.python.org/downloads/
 
+You can run the program using ```py .\Change_Language.py```
 
 
+The program has a few prompts to help understand what is needed from the user.
+
+Specifically, the program will first check if the current language selected has been saved locally.
+If not, it will ask if you want to locally save the current language selected.
+
+When the game saves these files, it will create a new directory outside of the packages directory.
+By default, this directory is:
+
+*C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages*
+
+Note: If this is not your path to the Destiny 2 packages, you should change line 5 in the Change_Language.py file to the correct path.
 
 
-
-
+*If you need to exit the program at any point, just hold Control then press C*
 
 
 
